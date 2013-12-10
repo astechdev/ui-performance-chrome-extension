e38449be3beec0b03b8fa0bb9a43408af92a1888
Sam Artioli <sam.artioli@disney.com>
2013-12-10 18:36:37 -0500

    working

--- background.js
+++ background.js
@@ -3,85 +3,238 @@
 // found in the LICENSE file.
 
 
-var filePath = 'file://localhost/Users/sartioli/Desktop/XUP-Perf/';
+var url = 'https://wdw-stage.disney.go.com/';
 
-var files = [
-    'ak_xup_false_post_avail.htm',
-    'ak_xup_false_pre_avail.htm',
-    'ak_xup_true_post_avail.htm',
-    'ak_xup_true_pre_avail.htm',
-    'resorts_xup_false_post_avail.htm',
-    'resorts_xup_false_pre_avail.htm',
-    'resorts_xup_true_post_avail.htm',
-    'resorts_xup_true_pre_avail.htm'
+var pages = [
+    {
+        page: 'resorts/',
+        description: 'resorts xup=false pre-avail',
+        cookies: []
+    },
+    {
+        page: 'resorts/',
+        description: 'resorts xup=true pre-avail',
+        cookies: [
+            {
+                url: url,
+                name: 'enableLodgingXUp',
+                value: encodeURI('{"enableLodgingXUp":true}'),
+                path: '/'
+            }
+        ]
+    },
+    {
+        page: 'resorts/',
+        description: 'resorts xup=true post-avail',
+        cookies: [
+            {
+                url: url,
+                name: 'enableLodgingXUp',
+                value: encodeURI('{"enableLodgingXUp":true}'),
+                path: '/'
+            },{
+                url: url,
+                name: 'roomForm_jar',
+                value: encodeURI('{"checkInDate":"2014-02-01","checkOutDate":"2014-02-07","numberOfAdults":"2","numberOfChildren":"0","accessible":"0","resort":""}'),
+                path: '/'
+            }
+        ]
+    },
+    {
+        page: 'resorts/animal-kingdom-lodge/rates-rooms/',
+        description: 'ak lodge xup=false pre-avail',
+        cookies: []
+    },
+    {
+        page: 'resorts/animal-kingdom-lodge/rates-rooms/',
+        description: 'ak lodge xup=true pre-avail',
+        cookies: [
+            {
+                url: url,
+                name: 'enableLodgingXUp',
+                value: encodeURI('{"enableLodgingXUp":true}'),
+                path: '/'
+            }
+        ]
+    },
+    {
+        page: 'resorts/animal-kingdom-lodge/rates-rooms/',
+        description: 'ak lodge xup=true post-avail',
+        cookies: [
+            {
+                url: url,
+                name: 'enableLodgingXUp',
+                value: encodeURI('{"enableLodgingXUp":true}'),
+                path: '/'
+            },{
+                url: url,
+                name: 'roomForm_jar',
+                value: encodeURI('{"checkInDate":"2014-02-01","checkOutDate":"2014-02-07","numberOfAdults":"2","numberOfChildren":"0","accessible":"0","resort":"80010395;entityType=resort"}'),
+                path: '/'
+            }
+        ]
+    }
 ];
 
-var files_iterator = 0;
+var pages_iterator = 0;
 
 var repetitions = 10;
 var rep_iterator = 1;
 
-var stats = {};
+var stats = [];
 
-var updateUrl = function(tabId) {
+var setupPage = function(tabId) {
 
-    if (files_iterator < files.length) {
+    if (pages_iterator < pages.length) {
+        console.log('### INFO: Setting Up Page');
 
-        if (rep_iterator == 1) {
-            stats[files[files_iterator]] = { download: 0, processing: 0 };
-        }
+        //Remove Cookies
+        chrome.cookies.getAll({
+            url: url
+        }, function(cookies) {
+            cookies.forEach(function(cookie) {
+                chrome.cookies.remove({
+                    url: url,
+                    name: cookie.name,
+                    storeId: cookie.storeId
+                });
+            });
+            console.log("### INFO: Done Removing Cookies");
 
-        chrome.tabs.update(tabId, {url: filePath + files[files_iterator]});
+            //Set Cookies
+            pages[pages_iterator].cookies.forEach(function(cookie) {
+                chrome.cookies.set(cookie);
+                console.log("### INFO: Done Adding Cookies");
+            });
 
-    } else {
+            //Reload and start test
+            chrome.tabs.update(tabId, {url: url}, function(tab) {
 
-        chrome.tabs.onUpdated.removeListener(listener);
-        console.log(stats);
-    }
+                console.log("### INFO: Starting Test | Page: %s%s \n### INFO: Description: '%s'\n" +
+                    "download, domInteractive, loadEventStart, loadEventEnd",
+                    url, pages[pages_iterator].page, pages[pages_iterator].description);
 
-};
-
-var listener = function (tabId, changeInfo, tab) {
-
-        if (changeInfo.status == 'complete') {
-            //alert(window.performance.timing.domInteractive);
-
-            chrome.tabs.executeScript(tabId, {file: "content.js"}, function(results) {
-
-                if (files_iterator == 1) {
-                    stats[files[files_iterator]].download = results[0].download;
-                    stats[files[files_iterator]].processing = results[0].processing;
-                } else {
-                    stats[files[files_iterator]].download = (stats[files[files_iterator]].download + results[0].download)/2;
-                    stats[files[files_iterator]].processing = (stats[files[files_iterator]].processing + results[0].processing)/2;
-                }
-                //console.log(stats);
-
-                if (rep_iterator >= repetitions) {
-                    files_iterator++;
-                    rep_iterator = 1;
-                } else {
-                    rep_iterator++;
-                }
-
-                updateUrl(tabId);
+                chrome.tabs.onUpdated.addListener(listener);
+                updateUrl(tab.id);
 
             });
 
-        }
+        });
+
+
+    } else {
+        console.log('### INFO: Done Iterating Through Pages');
+        console.log(stats);
+        //printReport();
+    }
+
+
+};
+
+var printReport = function() {
+
+    stats.foreach(function(elem, index, array) {
+
+        console.log(index);
+        console.log('    download');
+    });
+
+}
+
+var updateUrl = function(tabId) {
+
+    //console.log("### DEBUG: In updateUrl... | rep_iterator: %s | pages_iterator: %s ", rep_iterator, pages_iterator);
+
+    if (rep_iterator == 1) {
+        //First Time Through
+        stats[pages[pages_iterator].description] = {
+            download: 0, domInteractive: 0, loadEventStart: 0, loadEventEnd: 0,
+            fastest: {download: 0, domInteractive: 0, loadEventStart: 0, loadEventEnd: 0},
+            slowest: {download: 0, domInteractive: 0, loadEventStart: 0, loadEventEnd: 0}
+        };
+    }
+
+    chrome.tabs.update(tabId, {url: url + pages[pages_iterator].page});
+
+
+};
+
+
+var listener = function (tabId, changeInfo, tab) {
+
+    if (changeInfo.status == 'complete') {
+
+        chrome.tabs.executeScript(tabId, {file: "content.js", runAt: "document_idle"});
 
     }
 
+}
+
+var handleResponse = function(tabId, results) {
+
+    var desc = stats[pages[pages_iterator].description];
+
+    console.log('%s, %s, %s, %s',
+        results.download, results.domInteractive, results.loadEventStart, results.loadEventEnd );
+
+    if (rep_iterator == 1) {
+        desc.download = results.download;
+        desc.domInteractive = results.domInteractive;
+        desc.loadEventStart = results.loadEventStart;
+        desc.loadEventEnd = results.loadEventEnd;
+
+        desc.fastest.download = results.download;
+        desc.fastest.domInteractive = results.domInteractive;
+        desc.fastest.loadEventStart = results.loadEventStart;
+        desc.fastest.loadEventEnd = results.loadEventEnd;
+
+        desc.slowest.download = results.download;
+        desc.slowest.domInteractive = results.domInteractive;
+        desc.slowest.loadEventStart = results.loadEventStart;
+        desc.slowest.loadEventEnd = results.loadEventEnd;
+
+    } else {
+        desc.download = (desc.download + results.download)/2;
+        desc.domInteractive = (desc.domInteractive + results.domInteractive)/2;
+        desc.loadEventStart = (desc.loadEventStart + results.loadEventStart)/2;
+        desc.loadEventEnd = (desc.loadEventEnd + results.loadEventEnd)/2;
+
+        if (desc.fastest.download > results.download) desc.fastest.download = results.download;
+        if (desc.fastest.domInteractive > results.domInteractive) desc.fastest.domInteractive = results.domInteractive;
+        if (desc.fastest.loadEventStart > results.loadEventStart) desc.fastest.loadEventStart = results.loadEventStart;
+        if (desc.fastest.loadEventEnd > results.loadEventEnd) desc.fastest.loadEventEnd = results.loadEventEnd;
+
+        if (desc.fastest.download < results.download) desc.slowest.download = results.download;
+        if (desc.fastest.domInteractive < results.domInteractive) desc.slowest.domInteractive = results.domInteractive;
+        if (desc.fastest.loadEventStart < results.loadEventStart) desc.slowest.loadEventStart = results.loadEventStart;
+        if (desc.fastest.loadEventEnd < results.loadEventEnd) desc.slowest.loadEventEnd = results.loadEventEnd;
+    }
+    //console.log(stats);
+
+    if (rep_iterator >= repetitions) {
+        pages_iterator++;
+        rep_iterator = 1;
+        chrome.tabs.onUpdated.removeListener(listener);
+        setupPage(tabId);
+
+    } else {
+        rep_iterator++;
+        updateUrl(tabId);
+    }
+
+}
+
 chrome.browserAction.onClicked.addListener(function(tab) {
 
-    //chrome.tabs.executeScript(tab.id, {file: "content.js", runAt: "document_start"});
-
-    files_iterator = 0;
+    pages_iterator = 0;
     rep_iterator = 1;
 
-    chrome.tabs.onUpdated.addListener(listener);
+    chrome.runtime.onMessage.addListener(function(results) {
+        handleResponse(tab.id, results)
+    });
 
-    updateUrl(tab.id);
+    setupPage(tab.id);
+
 
 });
 
--- content.js
+++ content.js
@@ -1,19 +1,38 @@
-
-document.title="test";
-
 // document.addEventListener('PEP_LOADED', function (e) {
 //   console.log("PEP_LOADED");
 // }, false);
 
-var download = ((window.performance.timing.domLoading - window.performance.timing.navigationStart) / 1000);
-var processing = ((window.performance.timing.domInteractive - window.performance.timing.domLoading) / 1000);
+// $(document).bind( "PEP_LOADED", function( event ) {
+//     console.log('yes');
+// });
 
-console.log("download: " + download + " processing: " + processing);
+// $.event.trigger('PEP_LOADED');
 
+var checkLoadEventEnd = function() {
 
-var toReturn = {
-    download: download,
-    processing: processing
+    if(window.performance.timing.loadEventEnd == 0) {
+
+        console.log("### INFO: loadEventEnd is still 0");
+        setTimeout(checkLoadEventEnd, 500);
+
+    } else {
+
+        var download = ((window.performance.timing.domLoading - window.performance.timing.navigationStart) / 1000);
+        var domInteractive = ((window.performance.timing.domInteractive - window.performance.timing.domLoading) / 1000);
+        var loadEventStart = ((window.performance.timing.loadEventStart - window.performance.timing.domLoading) / 1000);
+        var loadEventEnd = ((window.performance.timing.loadEventEnd - window.performance.timing.domLoading) / 1000);
+
+        var toReturn = {
+            download: download,
+            domInteractive: domInteractive,
+            loadEventStart: loadEventStart,
+            loadEventEnd: loadEventEnd
+        };
+
+        chrome.runtime.sendMessage(toReturn);
+
+    }
+
 };
 
-toReturn;
+checkLoadEventEnd();
