1dabe5d29f6f36beee837f15d092d8c0fdefb542
Sam Artioli <sam.artioli@disney.com>
2013-12-10 20:38:22 -0500

    added more stats

--- background.js
+++ background.js
@@ -3,7 +3,8 @@
 // found in the LICENSE file.
 
 
-var url = 'https://wdw-stage.disney.go.com/';
+//var url = 'https://wdw-stage.disney.go.com/';
+var url = 'https://wdprolt02.disney.go.com/';
 
 var pages = [
     {
@@ -135,18 +136,63 @@ var printReport = function() {
 
     pages.forEach(function(elem) {
 
-        console.log("\n### %s%s ###\n### %s ###\n# AVERAGE #\n" +
-            "download, domInteractive, loadEventStart, loadEventEnd",
-            url, elem.page, elem.description);
-        console.log("%s, %s, %s, %s", elem.stats.download, elem.stats.domInteractive, elem.stats.loadEventStart, elem.stats.loadEventEnd);
+        // Calculate Average
+
+        var results = getAvgStdDevVar(elem.stats);
+
+
+        console.log("\n### %s%s ###\n### %s ###\ndownload, domInteractive, loadEventStart, loadEventEnd", url, elem.page, elem.description);
+        console.log("# AVERAGE #");
+        console.log("%s, %s, %s, %s", elem.stats.average.download, elem.stats.average.domInteractive, elem.stats.average.loadEventStart, elem.stats.average.loadEventEnd);
         console.log("# FASTEST #");
         console.log("%s, %s, %s, %s", elem.stats.fastest.download, elem.stats.fastest.domInteractive, elem.stats.fastest.loadEventStart, elem.stats.fastest.loadEventEnd);
         console.log("# SLOWEST #");
         console.log("%s, %s, %s, %s", elem.stats.slowest.download, elem.stats.slowest.domInteractive, elem.stats.slowest.loadEventStart, elem.stats.slowest.loadEventEnd);
+        console.log("# STANDARD DEVIATION #");
+        console.log("%s, %s, %s, %s", elem.stats.deviation.download, elem.stats.deviation.domInteractive, elem.stats.deviation.loadEventStart, elem.stats.deviation.loadEventEnd);
+        console.log("# VARIANCE #");
+        console.log("%s, %s, %s, %s", elem.stats.variance.download, elem.stats.variance.domInteractive, elem.stats.variance.loadEventStart, elem.stats.variance.loadEventEnd);
 
     });
 
 }
+
+var getAvgStdDevVar = function(stats) {
+
+    var dl = [], di = [], les = [], lee = [];
+
+    for (var i=0; i<stats.runs.length; i++) {
+
+        dl.push(stats.runs[i].download);
+        di.push(stats.runs[i].domInteractive);
+        les.push(stats.runs[i].loadEventStart);
+        lee.push(stats.runs[i].loadEventEnd);
+
+    }
+
+    stats.average = {
+        download: getAverageFromNumArr(dl, 3),
+        domInteractive: getAverageFromNumArr(di, 3),
+        loadEventStart: getAverageFromNumArr(les, 3),
+        loadEventEnd: getAverageFromNumArr(lee, 3)
+    };
+
+    stats.deviation = {
+        download: getStandardDeviation(dl, 3),
+        domInteractive: getStandardDeviation(di, 3),
+        loadEventStart: getStandardDeviation(les, 3),
+        loadEventEnd: getStandardDeviation(lee, 3)
+    };
+
+    stats.variance =  {
+        download: getVariance(dl, 3),
+        domInteractive: getVariance(di, 3),
+        loadEventStart: getVariance(les, 3),
+        loadEventEnd: getVariance(lee, 3)
+    };
+
+};
+
 
 var updateUrl = function(tabId) {
 
@@ -156,7 +202,10 @@ var updateUrl = function(tabId) {
         //First Time Through
 
         pages[pages_iterator].stats = {
-            download: 0, domInteractive: 0, loadEventStart: 0, loadEventEnd: 0,
+            runs: [],
+            // average: {download: 0, domInteractive: 0, loadEventStart: 0, loadEventEnd: 0},
+            // deviation: {download: 0, domInteractive: 0, loadEventStart: 0, loadEventEnd: 0},
+            // variance: {download: 0, domInteractive: 0, loadEventStart: 0, loadEventEnd: 0},
             fastest: {download: 0, domInteractive: 0, loadEventStart: 0, loadEventEnd: 0},
             slowest: {download: 0, domInteractive: 0, loadEventStart: 0, loadEventEnd: 0}
         };
@@ -185,11 +234,15 @@ var handleResponse = function(tabId, results) {
     console.log('%s, %s, %s, %s',
         results.download, results.domInteractive, results.loadEventStart, results.loadEventEnd );
 
+    stats.runs[(rep_iterator-1)] = {download: 0, domInteractive: 0, loadEventStart: 0, loadEventEnd: 0};
+
+    stats.runs[(rep_iterator-1)].download = results.download;
+    stats.runs[(rep_iterator-1)].domInteractive = results.domInteractive;
+    stats.runs[(rep_iterator-1)].loadEventStart = results.loadEventStart;
+    stats.runs[(rep_iterator-1)].loadEventEnd = results.loadEventEnd;
+
+
     if (rep_iterator == 1) {
-        stats.download = results.download;
-        stats.domInteractive = results.domInteractive;
-        stats.loadEventStart = results.loadEventStart;
-        stats.loadEventEnd = results.loadEventEnd;
 
         stats.fastest.download = results.download;
         stats.fastest.domInteractive = results.domInteractive;
@@ -202,20 +255,16 @@ var handleResponse = function(tabId, results) {
         stats.slowest.loadEventEnd = results.loadEventEnd;
 
     } else {
-        stats.download = (stats.download + results.download)/2;
-        stats.domInteractive = (stats.domInteractive + results.domInteractive)/2;
-        stats.loadEventStart = (stats.loadEventStart + results.loadEventStart)/2;
-        stats.loadEventEnd = (stats.loadEventEnd + results.loadEventEnd)/2;
 
         if (stats.fastest.download > results.download) stats.fastest.download = results.download;
         if (stats.fastest.domInteractive > results.domInteractive) stats.fastest.domInteractive = results.domInteractive;
         if (stats.fastest.loadEventStart > results.loadEventStart) stats.fastest.loadEventStart = results.loadEventStart;
         if (stats.fastest.loadEventEnd > results.loadEventEnd) stats.fastest.loadEventEnd = results.loadEventEnd;
 
-        if (stats.fastest.download < results.download) stats.slowest.download = results.download;
-        if (stats.fastest.domInteractive < results.domInteractive) stats.slowest.domInteractive = results.domInteractive;
-        if (stats.fastest.loadEventStart < results.loadEventStart) stats.slowest.loadEventStart = results.loadEventStart;
-        if (stats.fastest.loadEventEnd < results.loadEventEnd) stats.slowest.loadEventEnd = results.loadEventEnd;
+        if (stats.slowest.download < results.download) stats.slowest.download = results.download;
+        if (stats.slowest.domInteractive < results.domInteractive) stats.slowest.domInteractive = results.domInteractive;
+        if (stats.slowest.loadEventStart < results.loadEventStart) stats.slowest.loadEventStart = results.loadEventStart;
+        if (stats.slowest.loadEventEnd < results.loadEventEnd) stats.slowest.loadEventEnd = results.loadEventEnd;
     }
     //console.log(stats);
 
