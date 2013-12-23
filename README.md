0d43a6bfd9f8b3c11de4020023289e187a7cc2e6
Sam Artioli <sam.artioli@disney.com>
2013-12-23 11:33:39 -0500

    Updating ReadMe

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Clone the code:
 
     git clone git://github.disney.com/sartioli/ui-performance-chrome-extension.git
 
+## Installation
+
 Open Chrome Extensions Tab (Window->Extensions)
 
 Click on "Developer Mode"
@@ -19,4 +21,21 @@ Make sure you check: "Allow access to file URLs" for the "UI Performace Tool" ex
 
 If you make changes to any of the files, you will want to click the "Reload" button the "UI Performace Tool" extension
 
-To see the output of the tool. Click on the "Inspect views: background page" link in the "UI Performace Tool" extension
+## Output
+
+To see the output of the tool, click on the "Inspect views: background page" link in the "UI Performace Tool" extension (See Image Above)
+
+You will see progressive output, as well as a text dump that can be imported or pasted into excell as csv data.
+
+## Customization
+
+All changes should be made in settings.js
+
+## External References
+
+[W3 Processing Model](http://www.w3.org/TR/navigation-timing/#processing-model)
+
+[How not to trigger a layout in webkit](http://gent.ilcore.com/2011/03/how-not-to-trigger-layout-in-webkit.html)
+
+[Rendering Repaint Reflow Relayout Restyle](http://www.phpied.com/rendering-repaint-reflowrelayout-restyle/)
+
--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ var url = 'https://wdw-stage.disney.go.com/';
 //var url = 'https://wdprolt02.disney.go.com/';
 //var url = 'https://disneyworld.disney.go.com/';
 
-var repetitions = 1;
+var repetitions = 30;
 
 var pages = [
     {
@@ -22,79 +22,79 @@ var pages = [
             }
         ]
     },
-    // {
-    //     page: 'resorts/',
-    //     description: 'resorts xup=true pre-avail',
-    //     cookies: [
-    //         {
-    //             url: url,
-    //             name: 'enableLodgingXUp',
-    //             value: encodeURI('{"enableLodgingXUp":true}'),
-    //             path: '/'
-    //         }
-    //     ]
-    // },
-    // {
-    //     page: 'resorts/',
-    //     description: 'resorts xup=true post-avail',
-    //     cookies: [
-    //         {
-    //             url: url,
-    //             name: 'enableLodgingXUp',
-    //             value: encodeURI('{"enableLodgingXUp":true}'),
-    //             path: '/'
-    //         },{
-    //             url: url,
-    //             name: 'roomForm_jar',
-    //             value: encodeURI('{"checkInDate":"2014-02-01","checkOutDate":"2014-02-07","numberOfAdults":"2","numberOfChildren":"0","accessible":"0","resort":""}'),
-    //             path: '/'
-    //         }
-    //     ]
-    // },
-    // {
-    //     page: 'resorts/animal-kingdom-lodge/rates-rooms/',
-    //     description: 'ak lodge xup=false pre-avail',
-    //     cookies: []
-    // },
-    // {
-    //     page: 'resorts/animal-kingdom-lodge/rates-rooms/',
-    //     description: 'ak lodge xup=false post-avail',
-    //     cookies: [
-    //         {
-    //             url: url,
-    //             name: 'roomForm_jar',
-    //             value: encodeURI('{"checkInDate":"2014-02-01","checkOutDate":"2014-02-07","numberOfAdults":"2","numberOfChildren":"0","accessible":"0"}'),
-    //             path: '/'
-    //         }
-    //     ]
-    // },
-    // {
-    //     page: 'resorts/animal-kingdom-lodge/rates-rooms/',
-    //     description: 'ak lodge xup=true pre-avail',
-    //     cookies: [
-    //         {
-    //             url: url,
-    //             name: 'enableLodgingXUp',
-    //             value: encodeURI('{"enableLodgingXUp":true}'),
-    //             path: '/'
-    //         }
-    //     ]
-    // },
-    // {
-    //     page: 'resorts/animal-kingdom-lodge/rates-rooms/',
-    //     description: 'ak lodge xup=true post-avail',
-    //     cookies: [
-    //         {
-    //             url: url,
-    //             name: 'enableLodgingXUp',
-    //             value: encodeURI('{"enableLodgingXUp":true}'),
-    //             path: '/'
-    //         },{
-    //             url: url,
-    //             name: 'roomForm_jar',
-    //             value: encodeURI('{"checkInDate":"2014-02-01","checkOutDate":"2014-02-07","numberOfAdults":"2","numberOfChildren":"0","accessible":"0","resort":"80010395;entityType=resort"}'),
-    //             path: '/'
-    //         }
-    //     ]
-    // }
+    {
+        page: 'resorts/',
+        description: 'resorts xup=true pre-avail',
+        cookies: [
+            {
+                url: url,
+                name: 'enableLodgingXUp',
+                value: encodeURI('{"enableLodgingXUp":true}'),
+                path: '/'
+            }
+        ]
+    },
+    {
+        page: 'resorts/',
+        description: 'resorts xup=true post-avail',
+        cookies: [
+            {
+                url: url,
+                name: 'enableLodgingXUp',
+                value: encodeURI('{"enableLodgingXUp":true}'),
+                path: '/'
+            },{
+                url: url,
+                name: 'roomForm_jar',
+                value: encodeURI('{"checkInDate":"2014-02-01","checkOutDate":"2014-02-07","numberOfAdults":"2","numberOfChildren":"0","accessible":"0","resort":""}'),
+                path: '/'
+            }
+        ]
+    },
+    {
+        page: 'resorts/animal-kingdom-lodge/rates-rooms/',
+        description: 'ak lodge xup=false pre-avail',
+        cookies: []
+    },
+    {
+        page: 'resorts/animal-kingdom-lodge/rates-rooms/',
+        description: 'ak lodge xup=false post-avail',
+        cookies: [
+            {
+                url: url,
+                name: 'roomForm_jar',
+                value: encodeURI('{"checkInDate":"2014-02-01","checkOutDate":"2014-02-07","numberOfAdults":"2","numberOfChildren":"0","accessible":"0"}'),
+                path: '/'
+            }
+        ]
+    },
+    {
+        page: 'resorts/animal-kingdom-lodge/rates-rooms/',
+        description: 'ak lodge xup=true pre-avail',
+        cookies: [
+            {
+                url: url,
+                name: 'enableLodgingXUp',
+                value: encodeURI('{"enableLodgingXUp":true}'),
+                path: '/'
+            }
+        ]
+    },
+    {
+        page: 'resorts/animal-kingdom-lodge/rates-rooms/',
+        description: 'ak lodge xup=true post-avail',
+        cookies: [
+            {
+                url: url,
+                name: 'enableLodgingXUp',
+                value: encodeURI('{"enableLodgingXUp":true}'),
+                path: '/'
+            },{
+                url: url,
+                name: 'roomForm_jar',
+                value: encodeURI('{"checkInDate":"2014-02-01","checkOutDate":"2014-02-07","numberOfAdults":"2","numberOfChildren":"0","accessible":"0","resort":"80010395;entityType=resort"}'),
+                path: '/'
+            }
+        ]
+    }
 ];
